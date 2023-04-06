98fa50a996c12eafa290bf801ebfe272051855d3
João-Medina <jvmedina2003@gmail.com>
2023-04-05 22:03:14 -0300

    atualizado

---
+++ byteBank/ContaCorrente.cs
@@ -0,0 +1,53 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace byteBank
+{
+    public class ContaCorrente
+    {
+        public string titular = "...";
+        public string conta = "...";
+        public int numeroAgencia = 0;
+        public double saldo = 0;
+
+        //métodos
+        public void Depositar(double valor)
+        {
+            this.saldo += valor;
+        }
+
+        public bool Sacar(double valor)
+        {
+            if(valor <= this.saldo)
+            {
+                this.saldo -= valor;
+                return true;
+            }
+            else
+            {
+                return false;
+            }
+        }
+
+        public void TransferirAgencia(int agencia)
+        {
+            this.numeroAgencia = agencia;
+        }
+
+        public bool TransferirDinheiro(double valor, ContaCorrente destino)
+        {
+            if (this.saldo >= valor && valor > 0){
+                this.Sacar(valor);
+                destino.Depositar(valor);
+                return true;
+            }
+            else
+            {
+                return false;
+            }
+        }
+    }
+}
---
+++ byteBank/Program.cs
@@ -0,0 +1,74 @@
+﻿using byteBank;
+
+ContaCorrente contaAndre = new ContaCorrente();
+contaAndre.titular = "André Silva";
+contaAndre.numeroAgencia = 15;
+contaAndre.conta = "1010-X";
+contaAndre.saldo = 100;
+
+Console.WriteLine("Títular: " + contaAndre.titular);
+Console.WriteLine("Agência: " + contaAndre.numeroAgencia);
+Console.WriteLine("Conta: " + contaAndre.conta);
+Console.WriteLine("Saldo: " + contaAndre.saldo);
+
+contaAndre.Depositar(100);
+
+Console.WriteLine("Saldo pós depósito: " + contaAndre.saldo);
+
+
+bool saque = contaAndre.Sacar(300);
+
+if(saque == true)
+{
+    Console.WriteLine("Saldo pós saque: " + contaAndre.saldo);
+}
+else
+{
+    Console.WriteLine("***Saldo insuficiente para realizar o saque***");
+}
+
+
+contaAndre.TransferirAgencia(16);
+Console.WriteLine("Agência pós transferência: " + contaAndre.numeroAgencia);
+
+Console.WriteLine("-");
+Console.WriteLine("-");
+Console.WriteLine("-");
+
+
+ContaCorrente contaMaria = new ContaCorrente();
+contaMaria.titular = "Maria Fibonacci";
+contaMaria.numeroAgencia = 20;
+contaMaria.conta = "1212-Y";
+contaMaria.saldo = 130;
+
+Console.WriteLine("Saldo da Maria incial: " + contaMaria.saldo);
+Console.WriteLine("Saldo do André inicial: " + contaAndre.saldo);
+
+bool transferencia = contaAndre.TransferirDinheiro(50, contaMaria);
+
+if(transferencia == true)
+{
+    Console.WriteLine("Saldo da Maria pós transferência: " + contaMaria.saldo);
+    Console.WriteLine("Saldo do André pós transferência: " + contaAndre.saldo);
+}
+
+else
+{
+    Console.WriteLine("***Saldo insuficiente para realizar transação***");
+}
+
+Console.WriteLine("-");
+Console.WriteLine("-");
+Console.WriteLine("-");
+
+
+ContaCorrente contaPedro = new ContaCorrente();
+Console.WriteLine(contaPedro.titular);
+Console.WriteLine(contaPedro.saldo);
+Console.WriteLine(contaPedro.numeroAgencia);
+Console.WriteLine(contaPedro.conta);
+
+
+Console.WriteLine("Pressione Enter para encerrar o programa: ");
+Console.ReadLine();
