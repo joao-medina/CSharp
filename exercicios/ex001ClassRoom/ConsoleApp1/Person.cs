412e16243474a5e99d60696b8df86d487df7671e
João-Medina <jvmedina2003@gmail.com>
2023-04-13 23:41:15 -0300

    atualizado

---
+++ exercicios/ex001ClassRoom/ConsoleApp1/Person.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ConsoleApp1
+{
+    internal class Person
+    {
+
+        private int age;
+
+        public void SetAge(int n)
+        {
+            age = n;
+        }
+
+        public void ShowAge()
+        {
+            Console.WriteLine("My age is " + this.age);
+        }
+
+    }
+}
---
+++ exercicios/ex001ClassRoom/ConsoleApp1/Program.cs
@@ -0,0 +1,23 @@
+﻿using ConsoleApp1;
+using System;
+
+class Programa
+{
+   static void Main(string[] args){
+
+        Person myPerson = new Person();
+
+        Studant myStudant = new Studant();
+        myStudant.GoToClasses();
+        myStudant.SetAge(13);
+        myStudant.ShowAge();
+
+        Teacher myTeacher = new Teacher();
+        myTeacher.Explain();
+        myTeacher.SetAge(25);
+        myTeacher.ShowAge();
+
+        Console.WriteLine("Press Enter: ");
+        Console.ReadLine();
+    }
+}
