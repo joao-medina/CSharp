58d6f9379ad9cff35a029b709affb01fc191eddc
João-Medina <jvmedina2003@gmail.com>
2023-04-14 14:20:10 -0300

    atualizado

---
+++ exercicios/ex002PhotoAlbum/ex002PhotoAlbum/Program.cs
@@ -0,0 +1,15 @@
+﻿using ex002PhotoAlbum;
+using System;
+
+class Programa
+{
+    static void Main(string[] args)
+    {
+        PhotoAlbum myPhotoAlbum = new PhotoAlbum();
+        int numberOfPages = myPhotoAlbum.GetNumberOfPages();
+        Console.WriteLine("Number of pages: " + numberOfPages);
+
+        Console.WriteLine("Press Enter: ");
+        Console.ReadLine();
+    }
+}
